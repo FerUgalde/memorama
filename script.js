43b5d0850769aebe228317c3aaf53364064b6637
FerUgalde <fernanda.ugalde@uabc.edu.mx>
2024-04-03 16:15:24 -0700

    solo voltear 2 cartas - mostrar modal con el match correcto - voltear cartas si el match es falso

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
-var cardState = false;
-let cards=[];
-
-class Card{
-    constructor(){
-
-    }
-}
+var cardsFlipped = [];
+let matches = [];
 
 function flipCard(card) {
-    card.classList.toggle('flip');
-    cardState = !cardState;
-    console.log(cardState);
+  if (cardsFlipped.length < 2) {
+    card.classList.toggle("flip");
+    cardsFlipped.push(card);
+  }
+  if (cardsFlipped.length === 2) {
+    matchVerify();
+  }
 }
 
+function matchVerify() {
+  if (cardsFlipped[0].dataset.framework === cardsFlipped[1].dataset.framework) {
+    matches.push(cardsFlipped[0].dataset.framework);
+    cardsFlipped.forEach((card) => {
+      card.setAttribute("disabled", true);
+      card.onclick = null;
+    });
+    cardsFlipped = [];
+    console.log("Done!");
+    showModal();
+  } else {
+    console.log("Nope");
+    setTimeout(() => {
+      cardsFlipped.forEach((card) => {
+        card.classList.toggle("flip");
+      });
+      cardsFlipped = [];
+    }, 1000);
+  }
+}
 
+function showModal() {
+  modal.style.display = "block";
+}
 // Obtén el modal
 var modal = document.getElementById("myModal");
-
-// Obtén el botón que abre el modal
-var btn = document.getElementById("openModalButton");
-
 // Obtén el elemento <span> que cierra el modal
 var span = document.getElementById("closeModalButton");
-
-// Cuando el usuario haga clic en el botón, abre el modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
 // Cuando el usuario haga clic en <span> (x), cierra el modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+span.onclick = function () {
+  modal.style.display = "none";
+};
 
 // Cuando el usuario haga clic en cualquier lugar fuera del modal, ciérralo
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// // Obtén el botón que abre el modal
+// var btn = document.getElementById("openModalButton");
+
+// // Cuando el usuario haga clic en el botón, abre el modal
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
